f33345b43348dde489a4fe262836ee4fd623f065
Vivek <vpandani1999@gmail.com>
2023-02-28 16:30:51 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test';
+
+  
+getNo(val: any){
+  console.log(val.target.value)
+}
+
+}
